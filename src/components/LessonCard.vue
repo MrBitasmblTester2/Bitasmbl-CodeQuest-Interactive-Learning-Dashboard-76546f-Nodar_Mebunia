547d2d2e0b5647fbad551ba2403a9be6d6c554c1
MrBitasmblTester2 <lulu2214@proton.me>
2025-11-19 12:22:08 +0400

    Add LessonCard.vue

---
+++ src/components/LessonCard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="lesson-card">
+    <h3>{{ lesson.title }}</h3>
+    <p>Level: {{ lesson.level }}</p>
+    <button @click="$emit('toggle', lesson.id)">{{ lesson.completed ? 'Reset' : (lesson.locked ? 'Locked' : 'Complete') }}</button>
+  </div>
+</template>
+<script setup>
+defineProps({ lesson: Object })
+</script>
