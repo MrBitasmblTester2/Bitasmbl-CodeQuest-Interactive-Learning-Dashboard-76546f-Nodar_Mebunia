72c33da3fee525ce8e09efb4325d31d31485ed39
MrBitasmblTester2 <lulu2214@proton.me>
2025-11-19 12:22:09 +0400

    Add LessonList.vue

---
+++ src/components/LessonList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <LessonCard v-for="l in lessons" :key="l.id" :lesson="l" @toggle="toggle" />
+  </div>
+</template>
+<script setup>
+import LessonCard from './LessonCard.vue'
+const props = defineProps({ lessons: Array })
+const emit = defineEmits(['update'])
+const toggle = id => emit('update', id)
+</script>
