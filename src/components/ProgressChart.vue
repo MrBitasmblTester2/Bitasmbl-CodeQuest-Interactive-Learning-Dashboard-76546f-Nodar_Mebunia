435001ab4b5213172ed9bfa1ccb53dc4142b67ce
MrBitasmblTester2 <lulu2214@proton.me>
2025-11-19 12:22:10 +0400

    Add ProgressChart.vue

---
+++ src/components/ProgressChart.vue
@@ -0,0 +1,10 @@
+<template>
+  <canvas ref="c" width="300" height="80"></canvas>
+</template>
+<script setup>
+import { onMounted, ref } from 'vue'
+import Chart from 'chart.js/auto'
+const props = defineProps({ data: Array })
+const c = ref(null)
+onMounted(()=>{ new Chart(c.value, { type: 'line', data: { labels: props.data.map((_,i)=>i+1), datasets:[{data:props.data, borderColor:'#3b82f6'}] } }) })
+</script>
